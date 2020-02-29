f52104599781f3fc36e423b46401bc051f0cb004
Michael Nalbandyan <xinger@ya.ru>
2020-02-29 20:00:42 +0300

    drawing with d3

--- src/components/Drawing.vue
+++ src/components/Drawing.vue
@@ -1,59 +1,70 @@
 <template>
-    <div class="drawing">
-        <canvas ref="drawing"></canvas>
-    </div>
+  <!--  https://codepen.io/uNickHow/pen/WOYBQP -->
+  <div class="drawing">
+    <svg id="svg" ref="drawing"></svg>
+  </div>
 </template>
 
 <script>
-    import Drawing from '@/classes/Drawing'
+  import Drawing from '@/classes/Drawing2';
+  import Mousetrap from 'mousetrap';
 
-    export default {
-        name: 'Drawing',
-        components: {},
-        props: {
-            thickness: {
-                type: Number,
-                default: 3
-            },
-            color: {
-                type: String,
-                default: 'rgba(0,0,0,1)'
-            }
-        },
-        computed: {
-
-        },
-        mounted() {
-            this.drawing = new Drawing(this.$refs.drawing, {
-                events: {
-                    start: () => {
-                        this.$emit('start');
-                    },
-                    stop: () => {
-                        this.$emit('stop');
-                    }
-                }
-            });
-        },
-        watch: {
-            thickness(val) {
-                this.drawing.thickness(val);
-            },
-            color(val) {
-                this.drawing.color(val);
-            }
+  export default {
+    name: 'Drawing',
+    components: {},
+    props: {
+      thickness: {
+        type: Number,
+        default: 3
+      },
+      color: {
+        type: String,
+        default: 'rgba(0,0,0,1)'
+      }
+    },
+    computed: {},
+    methods: {
+      undoHandler() {
+        this.drawing.undo();
+      }
+    },
+    mounted() {
+      this.drawing = new Drawing(this.$refs.drawing, {
+        events: {
+          start: () => {
+            this.$emit('start');
+          },
+          stop: () => {
+            this.$emit('stop');
+          }
         }
+      });
+
+      Mousetrap.bind('meta+z', this.undoHandler.bind(this));
+
+      this.drawing.thickness(this.thickness);
+      this.drawing.color(this.color);
+    },
+    watch: {
+      thickness(val) {
+        this.drawing.thickness(val);
+      },
+      color(val) {
+        this.drawing.color(val);
+      }
     }
+  }
 </script>
 
 <style lang="stylus">
-    .drawing
-        width 100%
-        height 100%
+  .drawing
+    width 100%
+    height 100%
 
-        canvas
-            display block
-            width 100%
-            height 100%
+    canvas,
+    svg
+      display block
+      width 100%
+      height 100%
 
 </style>
--- src/views/Sketch.vue
+++ src/views/Sketch.vue
@@ -1,197 +1,202 @@
 <template>
-    <div class="sketch">
-        <drawing
-            :thickness="parseInt(lineThickness)"
-            :color="lineColor"
-            @start="startDrawingHandler"
-            @stop="stopDrawingHandler"
-        ></drawing>
+  <div class="sketch">
+    <drawing
+        :thickness="parseInt(lineThickness)"
+        :color="lineColor"
+        @start="startDrawingHandler"
+        @stop="stopDrawingHandler"
+    ></drawing>
 
-        <div class="sketch__panel">
+    <div class="sketch__panel">
 
-            <div class="sketch__control">
-                <label>Thickness</label>
-                <input type="range" v-model="lineThickness" min="3" max="23" step="5">
-            </div>
+      <div class="sketch__control">
+        <label>Thickness</label>
+        <input type="range" v-model="lineThickness" min="3" max="23" step="5">
+      </div>
 
-            <div class="sketch__control">
-                <label>Opacity</label>
-                <input style="direction: rtl" type="range" v-model="opacity" min="0" max="100" step="10">
-            </div>
+      <div class="sketch__control">
+        <label>Opacity</label>
+        <input style="direction: rtl" type="range" v-model="opacity" min="0" max="100" step="10">
+      </div>
 
-            <div class="sketch__control" style="flex-direction: row">
-                <label>Color</label>
-                <div class="color"
-                     v-for="clr in colors"
-                     :style="{
+      <div class="sketch__control" style="flex-direction: row">
+        <label>Color</label>
+        <div class="color"
+             v-for="clr in colors"
+             :style="{
                         background: toRgb(clr)
                      }"
-                     @click="chooseColorHandler(clr)"
-                ></div>
+             @click="chooseColorHandler(clr)"
+        ></div>
 
-            </div>
+      </div>
 
-
-        </div>
 
     </div>
+
+  </div>
 </template>
 
 <script>
-    import Drawing from '@/components/Drawing'
-    import Mousetrap from 'mousetrap'
-    import {remote} from 'electron'
-    import http from 'http'
-    import fs from 'fs'
-    import path from 'path'
+  import Drawing from '@/components/Drawing'
+  import Mousetrap from 'mousetrap'
+  import {remote} from 'electron'
+  import * as ssvg from 'save-svg-as-png';
+  import http from 'http'
+  import fs from 'fs'
+  import path from 'path'
 
-    export default {
-        name: 'Sketch',
-        data: function() {
-            return {
-                lineThickness: 3,
-                opacity: 100,
-                colors: [
-                    [0, 0, 0],
-                    [242, 95, 92],
-                    [255, 224, 102],
-                    [36, 123, 160],
-                    [12, 193, 179]
-                ],
-                color: [0,0,0]
-            }
-        },
-        components: {
-            Drawing
-        },
-        props: {
+  export default {
+    name: 'Sketch',
+    data: function () {
+      return {
+        lineThickness: 3,
+        opacity: 100,
+        colors: [
+          [0, 0, 0],
+          [242, 95, 92],
+          [255, 224, 102],
+          [36, 123, 160],
+          [12, 193, 179]
+        ],
+        color: [0, 0, 0]
+      }
+    },
+    components: {
+      Drawing
+    },
+    props: {},
+    computed: {
+      lineColor() {
+        return this.toRgb(this.color, this.opacity / 100);
+      }
+    },
+    methods: {
+      chooseColorHandler(color) {
+        this.color = color;
+      },
 
-        },
-        computed: {
-            lineColor() {
-                return this.toRgb(this.color, this.opacity / 100);
-            }
-        },
-        methods: {
-            chooseColorHandler(color) {
-                this.color = color;
-            },
-
-            toRgb(color, alpha = null) {
-                if (color[3] === undefined) {
-                    color[3] = 1;
-                }
-
-                if (alpha !== null) {
-                    color[3] = alpha;
-                }
-
-                return `rgba(${color.join(',')})`;
-            },
-
-            startDrawingHandler() {
-                document.body.classList.add('drawing');
-            },
-
-            stopDrawingHandler() {
-                document.body.classList.remove('drawing');
-            },
-
-            saveImageHandler() {
-                const localPath = remote.app.getPath('desktop');
-
-                const savePath = remote.dialog.showSaveDialogSync({
-                    defaultPath: localPath
-                });
-            }
-        },
-        mounted() {
-            // Mousetrap.bind('meta+s', this.saveImageHandler.bind(this));
+      toRgb(color, alpha = null) {
+        if (color[3] === undefined) {
+          color[3] = 1;
         }
+
+        if (alpha !== null) {
+          color[3] = alpha;
+        }
+
+        return `rgba(${color.join(',')})`;
+      },
+
+      startDrawingHandler() {
+        document.body.classList.add('drawing');
+      },
+
+      stopDrawingHandler() {
+        document.body.classList.remove('drawing');
+      },
+
+      saveImageHandler() {
+        const localPath = remote.app.getPath('desktop');
+
+        ssvg.saveSvgAsPng(document.getElementById('svg'), 'sketch.png', {
+          backgroundColor: '#ffffff'
+        });
+
+        console.log(localPath);
+
+        // const savePath = remote.dialog.showSaveDialogSync({
+        //     defaultPath: localPath
+        // });
+      }
+    },
+    mounted() {
+      Mousetrap.bind('meta+s', this.saveImageHandler.bind(this));
     }
+  }
 </script>
 
 <style lang="stylus">
-    .sketch
-        width 100%
-        height 100%
+  .sketch
+    width 100%
+    height 100%
 
-        &__panel
-            position absolute
-            display flex
-            z-index 10
-            /*pointer-events none*/
-            user-select none
-            opacity 0.25
-            transition all 0.3s ease
-            width 100%
-            height 42px
-            bottom 0
-            padding 0 12px
-            box-sizing border-box
-            justify-content: flex-end;
-            border-top 1px solid rgba(0,0,0,0)
+    &__panel
+      position absolute
+      display flex
+      z-index 10
+      /*pointer-events none*/
+      user-select none
+      opacity 0.25
+      transition all 0.3s ease
+      width 100%
+      height 42px
+      bottom 0
+      padding 0 12px
+      box-sizing border-box
+      justify-content: flex-end;
+      border-top 1px solid rgba(0, 0, 0, 0)
 
-            &__container
-                display flex
-                border-radius 100px
-                height 60px
-                align-items center
-                justify-content right
+      &__container
+        display flex
+        border-radius 100px
+        height 60px
+        align-items center
+        justify-content right
 
-                input
-                    width 100px
-            &:hover
-                opacity 1
-                border-top 1px solid rgba(0,0,0,0.1)
-                background rgba(255,255,255,0.9)
+        input
+          width 100px
 
-        &__control
-            pointer-events auto
-            display flex
-            margin-left 30px
-            align-items center
+      &:hover
+        opacity 1
+        border-top 1px solid rgba(0, 0, 0, 0.1)
+        background rgba(255, 255, 255, 0.9)
 
-            label
-                margin-right 10px
-                letter-spacing 0.5px
+    &__control
+      pointer-events auto
+      display flex
+      margin-left 30px
+      align-items center
 
-    .color
-        width 16px
-        height 16px
-        border-radius 100%
-        margin-right 5px
-        border 1px solid rgba(0,0,0,0.1)
-        box-sizing border-box
-        box-shadow 0 1px 2px rgba(0,0,0,0.1)
+      label
+        margin-right 10px
+        letter-spacing 0.5px
 
-        &:last-child
-            margin-right 0
+  .color
+    width 16px
+    height 16px
+    border-radius 100%
+    margin-right 5px
+    border 1px solid rgba(0, 0, 0, 0.1)
+    box-sizing border-box
+    box-shadow 0 1px 2px rgba(0, 0, 0, 0.1)
 
-        &:hover
-            opacity 0.75
+    &:last-child
+      margin-right 0
 
-    input[type="range"]
-        -webkit-appearance none
-        padding 10px 0
-        background transparent
-        width 75px
+    &:hover
+      opacity 0.75
 
-        &:focus
-            outline none
+  input[type="range"]
+    -webkit-appearance none
+    padding 10px 0
+    background transparent
+    width 75px
 
-        &::-webkit-slider-runnable-track
-            height 2px
-            background rgba(0,0,0,0.1)
+    &:focus
+      outline none
 
-        &::-webkit-slider-thumb
-            -webkit-appearance: none;
-            width 10px
-            height 10px
-            background rgba(50,50,50,1)
-            margin-top -4px
-            border-radius 100%
+    &::-webkit-slider-runnable-track
+      height 2px
+      background rgba(0, 0, 0, 0.1)
 
+    &::-webkit-slider-thumb
+      -webkit-appearance: none;
+      width 10px
+      height 10px
+      background rgba(50, 50, 50, 1)
+      margin-top -4px
+      border-radius 100%
 
 
 </style>
